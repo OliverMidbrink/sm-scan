e16a8dadde11b21d6dc6980a24b6b1870abe4f02
Oliver Midbrink <oliver.midbrink@hotmail.com>
2021-10-13 23:32:03 +0200

    Added packages and made an MVP

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import QRCode from 'react-qr-code';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import OutlinedInput from '@mui/material/OutlinedInput';
+
+
+function App() {
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    mailPrefix: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-weight"
+            value={values.mailPrefix}
+            onChange={handleChange('mailPrefix')}
+            endAdornment={<InputAdornment position="end">@kth.se</InputAdornment>}
+            aria-describedby="outlined-weight-helper-text"
+            inputProps={{
+              'aria-label': 'weight',
+            }}
+          />
+          <FormHelperText id="outlined-weight-helper-text">KTH mail</FormHelperText>
+        </FormControl>
+        <QRCode value={values.mailPrefix} />
+        <p>
+          Fyll i din KTH mejl. Inga personuppgifter sparas.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
